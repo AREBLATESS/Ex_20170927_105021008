b13afda86c5cad47a61d17c1465594607c87f5cf
AREBLATESS <105021008@live.asia.edu.tw>
2017-09-27 12:04:12 +0800

    wwww

---
+++ 課表.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 2017/9/27
+ * Time: 上午 10:51
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AR's 課表</title>
+</head>
+    <body bgcolor="#ffe4c4">
+        <h1 align="center">hello world</h1>
+        <p align="center"><del><span style="font-size: 60px;color:blue">A</span><span style="font-size: 80px;color: red">s</span><span style="font-size: 92px;color: yellow">i</span><span style="font-size: 72px">a</span><span style="font-size: 80px;color:blue">U</span><span style="font-size: 100px">n</span><span style="font-size: 120px;color:blue">i</span><span style="font-size: 160px">v</span></del></p>
+        <table  width="300" align="center">
+            <tr >
+                <td><a href="index.html">首頁</a></td>
+                <td><a href="page1.html">hobby</a></td>
+                <td><a href="課表.php">課表</a></td>
+                <td><a href="page3.html">常用鏈結</a></td>
+            </tr>
+        </table>
+        <table border="1" width="600" align="center">
+            <tr height="40">
+                <th width="16%"></th>
+                <th width="16%">Mon</th>
+                <th width="16%">Tue</th>
+                <th width="16%">Wed</th>
+                <th width="16%">Thur</th>
+                <th width="16%">Fri</th>
+            </tr>
+
+
+
+            <tr height="40">
+                <td width="16%">8:10~9:00</td>
+                <td width="16%"></td>
+                <td width="16%" rowspan="2">英文閱讀與寫作</td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+            </tr>
+
+
+            <tr height="40">
+                <td width="16%">9:10~10:00</td>
+                <td width="16%"></td>
+                <td width="16%" rowspan="3">網頁系統開發(A)</td>
+                <td width="16%" rowspan="3">數位系統設計(A)</td>
+                <td width="16%"></td>
+            </tr>
+
+
+            <tr height="40">
+                <td width="16%">10:10~11:00</td>
+                <td width="16%">體育(三)</td>
+                <td width="16%" rowspan="2">視窗程式設計(A)</td>
+<!--            <td width="16%">網頁系統開發(A)</td>-->
+<!--            <td width="16%">數位系統設計(A)</td>-->
+                <td width="16%"></td>
+            </tr>
+
+            <tr height="40">
+                <td width="16%">11:10~12:00</td>
+                <td width="16%">體育(三)</td>
+<!--            <td width="16%">視窗程式設計(A)</td>-->
+<!--            <td width="16%">網頁系統開發(A)</td>-->
+<!--            <td width="16%">數位系統設計(A)</td>-->
+                <td width="16%"></td>
+            </tr>
+        </table>
+
+        <table border="1" width="600" align="center" bgcolor="#dc143c">
+            <tr height="40">
+                <td width="16%">12:10~13:00</td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+            </tr>
+        </table>
+
+        <table border="1" width="600" align="center">
+            <tr height="40">
+                <td width="16%">13:10~14:00</td>
+                <td width="16%"></td>
+                <td width="16%">視窗程式設計(A)</td>
+                <td width="16%" rowspan="3">資料結構(A)</td>
+                <td width="16%" rowspan="2">人際關係與溝通-2(B)</td>
+                <td width="16%"></td>
+            </tr>
+
+            <tr height="40">
+                <td width="16%">14:10~15:00</td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+<!--            <td width="16%">資料結構(A)</td>-->
+<!--            <td width="16%">人際關係與溝通-2(B)</td>-->
+                <td width="16%"></td>
+            </tr>
+
+
+            <tr height="40">
+                <td width="16%">15:10~16:00</td>
+                <td width="16%"></td>
+                <td width="16%" rowspan="2">多媒體導論(A)</td>
+<!--            <td width="16%">資料結構(A)</td>-->
+                <td width="16%"></td>
+                <td width="16%"></td>
+            </tr>
+
+            <tr height="40">
+                <td width="16%">15:10~16:00</td>
+                <td width="16%"></td>
+<!--            <td width="16%">多媒體導論(A)</td>-->
+                <td width="16%">學輔時間(二)(A)</td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+            </tr>
+
+            <tr height="40">
+                <td width="16%">16:10~17:00</td>
+                <td width="16%">多媒體導論(A)</td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+                <td width="16%"></td>
+            </tr>
+        </table>
+    </body>
+</html>
